b936c2a04d4e956ad6f8473635739fb3174b02cd
Akmxyyxd683 <akmalganteng683@gmail.com>
2025-11-05 16:38:59 +0700

    Commit kedua

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LayoutsController;
 
 // 🔹 Halaman utama langsung ke controller
-Route::get('/layouts', [LayoutsController::class, 'index'])->name('layouts.index');
+Route::get('/', [LayoutsController::class, 'index'])->name('layouts.index');
