dd1c7a14c40f5f550bf14aa66d671bea7e66f087
𝕬𝖑𝖎𝖋 𝕲𝖆𝖓𝖙𝖊𝖓𝖌 <briliantamam45@gmail.com>
2025-11-15 14:43:01 +0700

    cumit

--- resources/views/kamar/index.blade.php
+++ resources/views/kamar/index.blade.php
@@ -23,6 +23,7 @@
 .btn-masuk {
     background: #b89376; /* coklat muda seperti gambar */
     color: white;
+    border-color: black;
     padding: 8px  twenty 16px;
     border-radius: 10px;
 }
@@ -52,9 +53,8 @@
     z-index: 5; display: inline-flex; align-items: center; gap: 8px;
     padding: 8px 12px; font-size: 14px; font-weight: 600;
     color: #2b2b2b; background: #fff;
-    border-radius: 8px; text-decoration: none;
+    text-decoration: none;
     box-shadow: 0 4px 16px rgba(0,0,0,.16);
-    border: 1px solid rgba(0,0,0,.06);
 }
 .back svg { width:16px; height:16px }
 
